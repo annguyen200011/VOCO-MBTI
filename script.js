40124f314a8d329a5f18729e19d82e93a3cfcd09
Glitch (glitch-hello-website) <none>
2021-09-21 18:21:12 +0000

    🍨🛤 Checkpoint ./script.js:35838281/1620 ./quiz1.html:35838281/18 ./index.html:35838281/11 ./quiz3.html:35838281/6

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-var mbti="";
-var E =document.getElementById('E').value;
-var I =document.getElementById('I');
-var S =document.getElementById('S');
-var N =document.getElementById('N');
-var T =document.getElementById('T');
-var F =document.getElementById('F');
-var J =document.getElementById('J');
-var P =document.getElementById('P');
-E.onclick = add(E);
-I.onclick = add(I);
-S.onclick = add(S);
-N.onclick = add(N);
-T.onclick = add(T);
-F.onclick = add(F);
-J.onclick = add(J);
-P.onclick = add(P);
+var el = document.getElementById('E').value;
+el.onclick = showFoo(el);
 
 
-function add(cha) {
-  mbti+=cha;
-  alert(mbti);
-  if(length(mbti)==4){
-    alert(mbti);
-  }
+function showFoo(cha) {
+  alert(cha);
+  return true;
 }
+
 
 
 
