096b166739f5aded89d60af31b432f04099aeb46
Glitch (glitch-hello-website) <none>
2021-09-21 19:18:08 +0000

    🎂👞 Checkpoint ./script.js:35838281/49

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var mbti = 0;
-var qui1;
-var qui2;
-function quiz1(cha) {
+
+var qui1= function quiz1(cha) {
   if (cha == 1) {
     return 1;
   } else return 0;
 }
-qui1=
-function quiz2(cha) {
+
+var qui2= function quiz2(cha) {
   if (cha == 1) {
     return 10;
   } else return 0;
 }
 
+mbti=
