13d966959f367462007de7185a7e4b4c3fce5a5b
Glitch (glitch-hello-website) <none>
2021-09-22 03:59:12 +0000

    👐⛲️ Checkpoint ./script.js:35838281/1062 ./index.html:35838281/22

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 var clickedlinks = new Array();;
-document.addEventListener('DOMContentLoaded',()=>{
-  document.querySelector('quiz1').onclick = function(){
-    clickedlinks.push(this.value);
-  }
-})
-
+function quiz1(cha){
+  if(cha==1)
+    {
+      clickedlinks.push("E");
+      alert(JSON.stringify(clickedlinks));
+    }
+  else
+    {
+      clickedlinks.push("I");
+      alert(JSON.stringify(clickedlinks));
+    }
+    
+}
+//alert(JSON.stringify(clickedlinks));
+function quiz2(cha){
+  if(cha==1)
+    {
+      clickedlinks.push("S");
+      alert(JSON.stringify(clickedlinks));
+    }
+  else
+    {
+      clickedlinks.push("N");
+      alert(JSON.stringify(clickedlinks));
+    }
+    
+}
 alert(JSON.stringify(clickedlinks));
