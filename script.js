f53b9266b9536e6a01500b83e8665c1db1eee097
Glitch (glitch-hello-website) <none>
2021-09-22 03:02:34 +0000

    🚦🚞 Checkpoint ./ESTJ.html:35838281/8 ./quiz4.html:35838281/77 ./quiz3.html:35838281/29 ./script.js:35838281/91

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var mbti = 0;
+let mbti = 0;
 
-var qui1= function quiz1(cha) {
+function quiz1(cha) {
   if (cha == 1) {
-    return 1;
+    mbti+= 1;
   } else return 0;
 }
 
-var qui2= function quiz2(cha) {
+function quiz2(cha) {
   if (cha == 1) {
     return 10;
   } else return 0;
 }
 
-mbti=
+
+
