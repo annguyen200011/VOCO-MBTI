0917a4861515c18cc97e9d9c21550894a9cf50fd
Glitch (glitch-hello-website) <none>
2021-09-22 03:25:42 +0000

    🙏⛹ Checkpoint ./script.js:35838281/1118

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-let mbti = 0;
-let E = document.addEventListener("quiz1", quiz1).value;
-alert(E);
-function quiz1(cha) {
-  if (cha == 1) {
-    alert(cha);
-    return 1;
-  } else return 0;
+var clickedlinks = [];
+document.querySelectorAll("E").forEach((a)=>{
+ E.addEventListener("E",addtoarr);
+});
+
+
+function addtoarr(){
+  clickedlinks.push(this.value);
 }
 
-function quiz2(cha) {
-  if (cha == 1) {
-    mbti += 10;
-  }
-}
+alert(clickedlinks.value);
