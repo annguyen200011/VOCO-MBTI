d6f9d1383fcee1feca35ed00dcddfe7b54c4665c
Glitch (glitch-hello-website) <none>
2021-09-22 07:32:48 +0000

    🎆🙏 Checkpoint ./style.css:35838281/644 ./script.js:35838281/557 ./quiz.html:35838281/2

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ function quiz1() {
     "Câu hỏi 1: Sau 1 tuần căng thẳng, bạn thường dành thời gian cuối tuần để làm gì?";
   document.querySelector("#imgChange").src =
     "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F1.png?v=1632245118753";
-  
+  var answer=["Đi ra ngoài cùng bạn bè", "Ở nhà thư giản"];
+  document.addEventListener("DOMContentLoaded", function(){
+
+      document.querySelector('#answer1').value = answer[0];
+    d  ocument.querySelector('#answer2').value = answer[1];
+    }
+  )
 }
