45ed3c793962bd1521b418f3e0e215c3c1391491
Glitch (glitch-hello-website) <none>
2021-09-22 09:16:21 +0000

    💺🌺 Checkpoint ./script.js:35838281/875 ./quiz.html:35838281/96

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function quiz1() {
     "<a href='javascript:quiz2()' onclick='getE()'>Đi ra ngoài cùng bạn bè</a>";
   document.querySelector("#answer2").innerHTML =
     "<a href='javascript:quiz2()' onclick='getI()'>Ở nhà thư giản</a>";
+  document.querySelector("p").innerHTML ="";
 }
 
 function getE() {
@@ -30,6 +31,7 @@ function quiz2() {
     "<a href='javascript:quiz3()' onclick='getS()'>Bạn quan tâm vào hiện tại, thích các giải pháp đơn giản và thực tế</a>";
   document.querySelector("#answer2").innerHTML =
     "<a href='javascript:quiz3()' onclick='getN()'>Bạn thích mơ mộng về tương lai và dựa nhiều vào trực giác hơn là thông tin</a>";
+  document.querySelector("p").innerHTML ="";
 }
 function getS() {
   if(parseInt(MBTI/10)==0)
@@ -51,6 +53,7 @@ function quiz3() {
     "<a href='javascript:quiz4()' onclick='getT()'>Luôn tìm kiếm sự kiện và tính logic để đưa ra quyết định";
   document.querySelector("#answer2").innerHTML =
     "<a href='javascript:quiz4()' onclick='getF()'>Xem xét cảm xúc cá nhân và ảnh hưởng của một quyết định lên người khác trước khi đưa ra quyết định đó</a>";
+  document.querySelector("p").innerHTML ="";
 }
 function getT() {
   if(parseInt(MBTI/100)==0)
@@ -72,6 +75,7 @@ function quiz4() {
     "<a href='javascript:answer()' onclick='getJ()'>Chuẩn bị chu đáo quà tặng và quần áo sẽ bận</a>";
   document.querySelector("#answer2").innerHTML =
     "<a href='javascript:answer()' onclick='getP()'>Sao phải chuẩn bị? Tôi thích mọi thứ diễn ra tự nhiên và thoải </a>";
+  document.querySelector("p").innerHTML ="";
 }
 function getJ() {
   if(parseInt(MBTI/1000)==0)
@@ -83,10 +87,20 @@ function getP() {
 }
 
 function answer() {
-  document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là: " + MBTI;
-  
-  document.querySelector("#imgChange").src =
+  switch(MBTI)
+    {
+      case 0:
+        document.querySelector("#imgChange").src =
     "https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2Fillustration.svg?v=1618177344016";
+        document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là INFP";
+        document.querySelector("p").innerHTML =
+          "hello";
+        break;
+      case 1:
+        
+        
+    }
+  
 
   document.querySelector("#answer1").innerHTML = "<a></a>";
   document.querySelector("#answer2").innerHTML = "<a></a>";
