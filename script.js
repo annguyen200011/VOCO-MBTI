84e0ce4164695029f8295d21fc471b982e060842
Glitch (glitch-hello-website) <none>
2021-09-22 10:14:06 +0000

    🌼🌻 Checkpoint ./.glitch-assets:35838281/3344 ./script.js:35838281/2692

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function answer() {
   switch (MBTI) {
     case 0:
       document.querySelector("#imgChange").src =
-        "https://static.neris-assets.com/images/personality-types/headers/diplomats_Mediator_INFP_personality_header.svg";
+        "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F0003dbd8-9bad-4911-98f1-cbb01750d1c0.image.png?v=1632305016307";
     
       document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là INFP";
       document.querySelector("p").innerHTML =
@@ -104,17 +104,17 @@ function answer() {
       document.querySelector("p").innerHTML = "";
       break;
     case 100:
-      document.querySelector("#imgChange").src = "";
-      document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là INTP";
-      document.querySelector("p").innerHTML = "";
+      document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F4606e440-1a9c-4ebd-87a5-9c05b97a137b.image.png?v=1632305057067";
+      document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là INTP: Nhà tư duy";
+      document.querySelector("p").innerHTML = "Bạn thích những học thuyết và tin rằng mọi thứ đều có thể được phân tích và cải thiện, vì vậy họ không quan tâm đến thế giới trần tục và những điều thực tế khác – họ nghĩ rằng nó ít thú vị hơn so với những ý tưởng hoặc hành trình theo đuổi kiến thức.";
       break;
     case 101:
-      document.querySelector("#imgChange").src = "";
-      document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ENTP";
-      document.querySelector("p").innerHTML = "";
+      document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2Faecb77c9-079f-4b9c-b1fc-0af0ea8032ca.image.png?v=1632305215539";
+      document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ENTP: Người có tầm nhìn xa";
+      document.querySelector("p").innerHTML = "Nhóm tính cách này rất nhanh nhạy và độc đáo, điều này mang lại cho họ một lợi thế lớn trong các cuộc tranh luận, các lĩnh vực học thuật và chính trị. Tuy nhiên họ cũng có xu hướng làm rất tốt trong nhiều lĩnh vực khác đòi hỏi phải sẵn sàng thách thức các ý tưởng hiện có và tổ chức nhiều cuộc tranh luận.";
       break;
     case 110:
-      document.querySelector("#imgChange").src = "";
+      document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2Fc724e988-5534-4e75-8e3f-c40e88978931.image.png?v=1632304955624";
       document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ISTP";
       document.querySelector("p").innerHTML = "";
       break;
@@ -124,17 +124,17 @@ function answer() {
       document.querySelector("p").innerHTML = "";
       break;
     case 1000:
-      document.querySelector("#imgChange").src = "";
+      document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F9e483f09-7e2f-4d65-bbf2-9b6c696c9de4.image.png?v=1632305250908";
       document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là INFJ";
       document.querySelector("p").innerHTML = "";
       break;
     case 1001:
-      document.querySelector("#imgChange").src = "";
+      document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F414e3a1c-eafb-42b1-82cc-64baf30e548e.image.png?v=1632305318770";
       document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ENFJ";
       document.querySelector("p").innerHTML = "";
       break;
     case 1010:
-      document.querySelector("#imgChange").src = "";
+      document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F0d321081-ac82-4ddf-9820-bc3f281d7c07.image.png?v=1632304339199";
       document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ISFJ";
       document.querySelector("p").innerHTML = "";
       break;
@@ -149,9 +149,9 @@ function answer() {
       document.querySelector("p").innerHTML = "";
       break;
     case 1101:
-      document.querySelector("#imgChange").src = "";
-      document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ENTJ";
-      document.querySelector("p").innerHTML = "";
+      document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2Ff0e8034a-3e33-47db-ad1c-e8680f20b3a2.image.png?v=1632305436434";
+      document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ENTJ: Nhà điều hanh";
+      document.querySelector("p").innerHTML = "Nhóm tính cách này thường rất lôi cuốn, lý trí và nhạy bén vì họ rất giỏi trong việc lãnh đạo và truyền cảm hứng cho người khác, vì vậy nhóm ENTJ có khả năng lãnh đạo tốt nhất trong tất cả các loại tính cách và họ tin rằng nếu có quyết tâm, mọi thứ đều có thể.";
       break;
     case 1110:
       document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2Fa2a90f65-0761-4234-8d1f-87745d06b6f9.image.png?v=1632304557705";
@@ -160,8 +160,8 @@ function answer() {
       break;
     case 1111:
       document.querySelector("#imgChange").src = "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F18056633-8c0a-474a-9341-ae9ca32d580c.image.png?v=1632304264503";
-      document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ESTJ";
-      document.querySelector("p").innerHTML = "";
+      document.querySelector("h2").innerHTML = "Nhóm tích cách của bạn là ESTJ: Người giám sát";
+      document.querySelector("p").innerHTML = "Họ là những người thiên về nguyên tắc, truyền thống, sự ổn định và họ cảm thấy cần phải gắn kết với điều gì đó – đó có thể là một gia đình, một cộng đồng hoặc một nhóm xã hội khác; vì vậy họ thích sự tổ chức của người khác và đảm bảo rằng họ sẽ tuân thủ các quy tắc truyền thống mà được ban hành bởi những người có thẩm quyền.";
       break;
   }
 
