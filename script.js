acdc68a9b3552accfd5a53d15c0d1d3c9287755b
Glitch (glitch-hello-website) <none>
2021-09-22 03:14:35 +0000

    🍶👕 Checkpoint ./script.js:35838281/1409 ./quiz1.html:35838281/32 ./quiz2.html:35838281/21 ./quiz3.html:35838281/26 ./quiz4.html:35838281/21

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let mbti = 0;
-
+let E = document.addEventListener("quiz1", quiz1).value;
+alert(E);
 function quiz1(cha) {
   if (cha == 1) {
-    mbti+= 1;
+    alert(cha);
+    return 1;
   } else return 0;
 }
 
 function quiz2(cha) {
   if (cha == 1) {
-    return 10;
-  } else return 0;
+    mbti += 10;
+  }
 }
-
-
-
