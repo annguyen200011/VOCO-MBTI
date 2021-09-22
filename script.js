208c55f6fcfaefe78c8fdaa01924517d42227276
Glitch (glitch-hello-website) <none>
2021-09-22 07:56:14 +0000

    👴🛤 Checkpoint ./script.js:35838281/4949 ./index.html:35838281/50

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ function quiz1() {
     "Câu hỏi 1: Sau 1 tuần căng thẳng, bạn thường dành thời gian cuối tuần để làm gì?";
   document.querySelector("#imgChange").src =
     "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F1.png?v=1632245118753";
-  var answer=["Đi ra ngoài cùng bạn bè", "Ở nhà thư giản"];
-  document.addEventListener("DOMContentLoaded", function(){
 
-      document.querySelector('#answer1').innerHTML = "Đi ra ngoài cùng bạn bè";
-      document.querySelector('#answer2').value = "Ở nhà thư giản";
-    }
-  )
+  document.querySelector("#answer1").innerHTML = "<a href='javascript:quiz2()' onlick='quiz2()'>Đi ra ngoài cùng bạn bè</a>";
+  document.querySelector("#answer2").innerHTML = "<a href='javascript:quiz2()' onlick='quiz2()'>Ở nhà thư giản</a>";
+}
+function quiz2() {
+  document.querySelector("h2").innerHTML =
+    "Câu hỏi 2: Câu nào sau đây mô tả đúng về tính cách của bạn:";
+  document.querySelector("#imgChange").src =
+    "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F1.png?v=1632245118753";
+
+  document.querySelector("#answer1").innerHTML = "<a href='javascript:quiz3()' onlick='quiz2()'>Bạn quan tâm vào hiện tại, thích các giải pháp đơn giản và thực tế</a>";
+  document.querySelector("#answer2").innerHTML = "<a href='javascript:quiz3()' onlick='quiz2()'>Bạn thích mơ mộng về tương lai và dựa nhiều vào trực giác hơn là thông tin</a>";
+}
+function quiz3() {
+  document.querySelector("h2").innerHTML =
+    "Câu hỏi 3: Khi đưa ra một quyết định bạn thường";
+  document.querySelector("#imgChange").src =
+    "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F1.png?v=1632245118753";
+
+  document.querySelector("#answer1").innerHTML = "<a href='javascript:quiz4()' onlick='quiz2()'>Luôn tìm kiếm sự kiện và tính logic để đưa ra quyết định";
+  document.querySelector("#answer2").innerHTML = "<a href='javascript:quiz4()' onlick='quiz2()'>Xem xét cảm xúc cá nhân và ảnh hưởng của một quyết định lên người khác trước khi đưa ra quyết định đó</a>";
+}
+function quiz4() {
+  document.querySelector("h2").innerHTML =
+    "Câu hỏi 4: Nếu còn 2 tuần nữa là dự đám cưới của bạn thân, bạn sẽ";
+  document.querySelector("#imgChange").src =
+    "https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F1.png?v=1632245118753";
+
+  document.querySelector("#answer1").innerHTML = "<a href='javascript:MBTI()' onlick='quiz2()'>Chuẩn bị chu đáo quà tặng và quần áo sẽ bận</a>";
+  document.querySelector("#answer2").innerHTML = "<a href='javascript:MBTI()' onlick='quiz2()'>Sao phải chuẩn bị? Tôi thích mọi thứ diễn ra tự nhiên và thoải </a>";
+}
+function MBTI(){
+  
 }
