24a82d9b5d4749767440c24254ca69726d41c597
Glitch (glitch-hello-website) <none>
2021-09-22 04:36:35 +0000

    🏖📰 Checkpoint ./script.js:35838281/164

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
-}
+
 
 function quiz1()
 {
+        document.querySelector('img').innerHTML="https://cdn.glitch.com/69eb61fe-1076-43a5-80fc-df6b2454d550%2F1.png?v=1632245118753";       
         document.querySelector('h2').innerHTML="Hello";
 }
     
