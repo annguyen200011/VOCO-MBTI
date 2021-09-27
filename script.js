d5bc4ad5c0a8186c58026adfe4723a34b6037605
Glitch (glitch-hello-website) <none>
2021-09-27 04:24:54 +0000

    🍫💕 Checkpoint ./index.html:35838281/12987 ./script.js:35838281/311

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ function answer() {
   document.getElementById("footer").style.display = "block";
   document.querySelector("#start-button").innerHTML = "Làm lại";
   document.querySelector("i").innerHTML =
-    "Mẹo: Bạn có thể làm lại lần nữa để có tham khảo các nhóm tính cách khác! Ngoài ra bạn có thể tìm hiểu thêm và phát triển bản thân qua Fanpage(https://www.facebook.com/VOCOCenter) và Khóa học của VOCO(https://tesse.io/vococenter?tab=courses) ";
+    "Mẹo: Bạn có thể làm lại lần nữa để có tham khảo các nhóm tính cách khác!<br>Ngoài ra bạn có thể tìm hiểu bản thân ";
 }
 
 
